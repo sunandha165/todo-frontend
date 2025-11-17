1e44f23be2911a790e60994e47ca11bcece08ca6
Sunandha Mani Vajrapu <vajrapusunandhamani@gmail.com>
2025-11-17 17:46:14 +0530

    Connect frontend to render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,40 @@
-import axios from 'axios';
-const BASE = 'https://todo-backend-drqv.onrender.com/';
+import axios from "axios";
 
-const withAuth = (opts = {}) => {
-  const token = localStorage.getItem('token');
-  return token ? { ...opts, headers: { Authorization: `Bearer ${token}`, ...(opts.headers || {}) } } : opts;
-};
+// 🔥 Use your Render backend URL here
+const BASE = "https://todo-backend-drqv.onrender.com/api";
 
 // AUTH
-export const registerUser = (data) => axios.post(`${BASE}/auth/register`, data).then(r => r.data);
-export const loginUser = (data) => axios.post(`${BASE}/auth/login`, data).then(r => r.data);
+export const registerUser = (data) =>
+  axios.post(`${BASE}/auth/register`, data).then((res) => res.data);
+
+export const loginUser = (data) =>
+  axios.post(`${BASE}/auth/login`, data).then((res) => res.data);
 
 // TASKS
-export const getTasks = () => axios.get(`${BASE}/task`, withAuth()).then(r => r.data);
-export const createTask = (task) => axios.post(`${BASE}/task`, task, withAuth()).then(r => r.data);
-export const updateTask = (id, body) => axios.put(`${BASE}/task/${id}`, body, withAuth()).then(r => r.data);
-export const deleteTask = (id) => axios.delete(`${BASE}/task/${id}`, withAuth()).then(r => r.data);
+export const getTasks = () =>
+  axios
+    .get(`${BASE}/task`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
+    .then((res) => res.data);
+
+export const createTask = (task) =>
+  axios
+    .post(`${BASE}/task`, task, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
+    .then((res) => res.data);
+
+export const updateTask = (id, body) =>
+  axios
+    .put(`${BASE}/task/${id}`, body, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
+    .then((res) => res.data);
+
+export const deleteTask = (id) =>
+  axios
+    .delete(`${BASE}/task/${id}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    })
+    .then((res) => res.data);
